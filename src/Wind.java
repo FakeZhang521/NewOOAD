f1ad8755ec057407f935009090d699491d3e274a
ailishs <54990686+ailishs@users.noreply.github.com>
2022-02-10 21:01:58 -0700

    getters, setters, abstraction, encapsulation

--- src/Wind.java
+++ src/Wind.java
@@ -1,7 +1,20 @@
 class Flute extends Wind{
-    String type;
+    protected String type; //example of ABSTRACTION
+    public String getType() //example of getters and setter -> ENCAPSULATION
+    {
+        return type;
+    }
 }
 
 class Harmonica extends Wind{
-    String key;
+    protected String key;
+    public String getKey() //example of getters and setter -> ENCAPSULATION
+    {
+        return Key;
+    }
+
+    public void setKey(String inKey)
+    {
+        key = inKey;
+    }
 }
