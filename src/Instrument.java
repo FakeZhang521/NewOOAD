1b6bb6f666449208390de0c78dfcb81369287630
ailishs <54990686+ailishs@users.noreply.github.com>
2022-02-10 20:53:32 -0700

    added the electric value to Stringed class...

--- src/Instrument.java
+++ src/Instrument.java
@@ -1,5 +1,11 @@
-class Stringed extends Instrument{
-    String electric;
+class Stringed extends Instrument
+{
+    private String electric;
+    
+    public Stringed(String electric)
+    {
+        this.electric = electric;
+    }
 }
 
 class Wind extends Instrument{
